540fb0929f8b4476ec096d798c7e11707b54722a
kouheikishigami <kouhei19920522@gmail.com>
2021-08-27 00:49:38 +0900

    refactor the client

--- src/client.ts
+++ src/client.ts
@@ -1,45 +1,41 @@
-import Axios, { AxiosInstance, AxiosPromise } from 'axios';
+import Axios, { AxiosInstance } from 'axios';
 import CloudrecodingClientInterface, { AcquireResponse, StopResponse, UpdateResponse, UpdateLayoutResponse, StartResponse, AcquirePrams } from "./clientInterface";
 
 interface RecordingParams {
   plainCredentials: string,
   appid: string,
   baseUrl?: string,
-  mode: 'individual' | 'mix' | 'web',
 }
 
+type Mode = 'individual' | 'mix' | 'web'
+
 class CloudrecodingClient implements CloudrecodingClientInterface {
-  private recordingParams: RecordingParams = {
-    plainCredentials: '',
-    appid: '',
-    baseUrl: 'https://api.agora.io/v1/apps',
-    mode: 'individual',
-  }
-  axiosClient: AxiosInstance;
+  private apiClient: AxiosInstance;
 
   constructor(recordingParams: RecordingParams) {
-    this.setUpAxiosClient(recordingParams);
-    this.recordingParams
-  }
-  async acquire(acquirePrams: AcquirePrams): Promise<AcquireResponse> {
-    try {
-      const agoraRes = await this.axiosClient.post('/cloud_recording/acquire', acquirePrams);
-    } catch (e) {
-      return
-    }
+    this.setupAxiosClient(recordingParams);
   }
 
-  private setUpAxiosClient(recordingParams: RecordingParams) {
-    const buffer = Buffer.from(recordingParams.plainCredentials);
+  private setupAxiosClient(params: RecordingParams) {
+    const buffer = Buffer.from(params.plainCredentials);
     const authorizationHeader = {
       'Content-Type': 'application/json',
       Authorization: `Basic ${buffer.toString('base64')}`,
     }
 
-    this.axiosClient = Axios.create({
+    this.apiClient = Axios.create({
       headers: authorizationHeader,
-      baseURL: `${this.recordingParams.baseUrl}/${this.recordingParams.appid}`
+      baseURL: `${params.baseUrl ?? 'https://api.agora.io/v1/apps'}/${params.appid}`
     })
+  }
+
+  async acquire(acquirePrams: AcquirePrams): Promise<AcquireResponse> {
+    try {
+      const agoraRes = await this.apiClient.post('/cloud_recording/acquire', acquirePrams);
+      // TODO: create response
+    } catch (e) {
+      return
+    }
   }
 
   start(): Promise<StartResponse> {
